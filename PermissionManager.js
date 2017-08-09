dc5fc4a1cdb310eb9c60b1578164ddab4e5b383d
Dave Longley <dlongley@digitalbazaar.com>
2017-08-09 14:16:55 -0400

    Remove erroneous `let`.

--- PermissionManager.js
+++ PermissionManager.js
@@ -65,7 +65,7 @@ export class PermissionManager {
     // TODO: disallow more than one request at a time or pipeline them
     let status = await this.query(permissionDesc);
     if(status.state === 'prompt') {
-      let status = await this._request(permissionDesc);
+      status = await this._request(permissionDesc);
       this._validatePermissionStatus(status);
       await this.permissions.setItem(permissionDesc.name, status);
     }
