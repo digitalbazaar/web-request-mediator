dc2b3bc63f1502404450d2384298c9e3f7185cfe
Dave Longley <dlongley@digitalbazaar.com>
2023-01-26 13:52:05 -0500

    Attempt to address issues with brave permission checks.

--- WebRequestHandlersService.js
+++ WebRequestHandlersService.js
@@ -127,6 +127,7 @@ export class WebRequestHandlersService extends EventEmitter {
    * @return the origin storage.
    */
   static _getOriginStorage(requestType) {
+    console.log('_getOriginStorage called');
     return localforage.createInstance(_getOriginStorageConfig(requestType));
   }
 
@@ -141,6 +142,7 @@ export class WebRequestHandlersService extends EventEmitter {
    * @return the handler storage.
    */
   static _getHandlerStorage(requestType, origin) {
+    console.log('_getHandlerStorage called');
     return localforage.createInstance(
       _getHandlerStorageConfig(requestType, origin));
   }
@@ -154,6 +156,7 @@ export class WebRequestHandlersService extends EventEmitter {
    *           given request type.
    */
   static async _getAllRegistrations(requestType) {
+    console.log('_getAllRegistrations called');
     // asynchronously get a list of promises where each will resolve to the
     // registered handler URLs for a particular origin
     const registrations = [];
